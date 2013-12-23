426a119eb2cc9127a1444ab7133bf02a69c77b93
Maks Kukushkin <maks.kukushkin@gmail.com>
2013-12-23 09:20:18 +0200

    the hardest commit in the project

--- booking-office/src/test/java/com/kukushkin/booking/office/dao/AccountDaoTest.java
+++ booking-office/src/test/java/com/kukushkin/booking/office/dao/AccountDaoTest.java
@@ -1,44 +1,56 @@
 package com.kukushkin.booking.office.dao;
 
 import static org.junit.Assert.*;
-
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class AccountDaoTest {
+public class AccountDaoTest extends BaseTest {
+	private static AccountDaoImpl accountDaoImpl;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		accountDaoImpl = new AccountDaoImpl();
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		accountDaoImpl = null;
+	}
 
-//	@Test
-//	public void testSetPermissions() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testAddAdditionalPermissions() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testAdd() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testDeleteT() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testDeleteInt() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testUpdate() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testRead() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	public void testSetPermissions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAdditionalPermissions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRead() {
+		fail("Not yet implemented");
+	}
 
 }
