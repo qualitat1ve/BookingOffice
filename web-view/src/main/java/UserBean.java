88285d7a277b263c9937a6f3b965121b9d81a6de
Maks Kukushkin <maks.kukushkin@gmail.com>
2014-01-13 00:19:00 +0200

    Simple login page was added

---
+++ web-view/src/main/java/UserBean.java
@@ -0,0 +1,45 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UserBean {
+    private String user;
+    private String password;
+    private String text;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String checkUser(){
+        if(this.user.equals("user") && this.password.equals("123")){
+            return "success";
+        }else{
+            return "loginfailed";
+        }
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
